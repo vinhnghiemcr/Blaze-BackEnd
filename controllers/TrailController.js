ff9da2fb040c3ca80d38d0bc0121357ace199a5b
Jenna Leopold <jleopold424@gmail.com>
2022-04-15 10:54:37 -0400

    defined CreateTrail in TrailController

--- controllers/TrailController.js
+++ controllers/TrailController.js
@@ -2,18 +2,39 @@ const { Trail, Post, Comment } = require('../models')
 const middleware = require('../middleware')
 
 const GetTrailDetails = async (req, res) => {
-  const { trailId } = req.params
-  const trail = await Trail.findByPk(trailId)
-  const posts = await Post.findAll({ where: { trailId: trailId } })
+  try {
+    const trailId = parseInt(req.params.trailId)
+    const trail = await Trail.findByPk(trailId)
+    const posts = await Post.findAll({ where: { trailId: trailId } })
 
-  for await (const post of posts) {
-    const comments = await Comment.findAll({ where: { postId: post.id } })
-    post.comments = [...comments]
+    for await (const post of posts) {
+      const comments = await Comment.findAll({ where: { postId: post.id } })
+      post.comments = [...comments]
+    }
+    trail.posts = [...posts]
+    return res.status(200).json(trail)
+  } catch (error) {
+    throw error
   }
-  trail.posts = [...posts]
-  return res.status(200).json(trail)
+}
+
+const CreateTrail = async (req, res) => {
+  try {
+    const userId = parseInt(req.params.userId)
+    const stateId = parseInt(req.params.stateId)
+    let trailBody = {
+      userId,
+      stateId,
+      ...req.body
+    }
+    let trail = await Trail.create(trailBody)
+    return res.status(201).json(trail)
+  } catch (error) {
+    throw error
+  }
 }
 
 module.exports = {
-  GetTrailDetails
+  GetTrailDetails,
+  CreateTrail
 }
