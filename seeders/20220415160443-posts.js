3108992197c916ee3cc1074af2b0262ab9af90cf
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-15 11:54:56 -0600

    seed file updates

--- seeders/20220415160443-posts.js
+++ seeders/20220415160443-posts.js
@@ -8,9 +8,72 @@ module.exports = {
       {
         title: 'My Travel to a Park',
         content: 'Blah Blah Blah',
-        img: 'Image',
-        trailId: 3,
+        img: 'https://media.beam.usnews.com/22/f2/4609fa4747298709af9f1269bbd6/150710-hiking-stock.jpg',
+        trailId: 1,
         userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'My Travel to a Park today',
+        content: 'Blah Blah',
+        img: 'https://appalachiantrail.org/wp-content/uploads/2020/01/ATC_RP14251_DSC_5582-lpr.jpg',
+        trailId: 1,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'Adventure',
+        content: 'Blah Blah Blah Blah',
+        img: 'https://explore-share.imgix.net/wp-content/uploads/2018/02/hiking_romania.jpg',
+        trailId: 2,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'My Travel to a Park',
+        content: 'Blah Blah Blah',
+        img: 'https://media.beam.usnews.com/22/f2/4609fa4747298709af9f1269bbd6/150710-hiking-stock.jpg',
+        trailId: 2,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'My Travel to a Park today',
+        content: 'Blah Blah',
+        img: 'https://appalachiantrail.org/wp-content/uploads/2020/01/ATC_RP14251_DSC_5582-lpr.jpg',
+        trailId: 3,
+        userId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'Adventure',
+        content: 'Blah Blah Blah Blah',
+        img: 'https://explore-share.imgix.net/wp-content/uploads/2018/02/hiking_romania.jpg',
+        trailId: 3,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'My Travel to a Park',
+        content: 'Blah Blah Blah',
+        img: 'https://media.beam.usnews.com/22/f2/4609fa4747298709af9f1269bbd6/150710-hiking-stock.jpg',
+        trailId: 4,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        title: 'My Travel to a Park today',
+        content: 'Blah Blah',
+        img: 'https://appalachiantrail.org/wp-content/uploads/2020/01/ATC_RP14251_DSC_5582-lpr.jpg',
+        trailId: 4,
+        userId: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       }
--- seeders/20220415160511-comments.js
+++ seeders/20220415160511-comments.js
@@ -7,8 +7,72 @@ module.exports = {
     await queryInterface.bulkInsert('comments', [
       {
         content: 'Comment to test',
-        postId: DataTypes.INTEGER,
-        userId: DataTypes.INTEGER,
+        postId: 1,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'Comment to test',
+        postId: 2,
+        userId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'Comment to test',
+        postId: 3,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'Comment to test',
+        postId: 4,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      ,
+      {
+        content: 'Comment to test',
+        postId: 5,
+        userId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'Comment to test',
+        postId: 6,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'Comment to test',
+        postId: 7,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'Comment to test',
+        postId: 8,
+        userId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'Comment to test',
+        postId: 9,
+        userId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        content: 'Comment to test',
+        postId: 3,
+        userId: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       }
