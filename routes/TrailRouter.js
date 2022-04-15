ba20ce08a55035d84ea01cf1433f3096a26f505f
Jenna Leopold <jleopold424@gmail.com>
2022-04-15 10:17:34 -0400

    added routes/TrailRouter.js

---
+++ routes/TrailRouter.js
@@ -0,0 +1,18 @@
+const router = require('express').Router()
+const controller = require('../controllers/TrailController')
+const middleware = require('../middleware')
+
+router.get('/:trailId', controller.GetTrailDetails)
+router.post('/user/:userId/state/:stateId', controller.CreateTrail)
+router.put(
+  '/:trailId/user/:userId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdateTrail
+)
+router.delete(
+  '/:trailId/user/:userId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.DeleteTrail
+)
