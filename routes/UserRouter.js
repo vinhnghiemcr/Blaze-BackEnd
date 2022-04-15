6333cf68e1c842b4e8bfc628c559ea7f11618ddd
Jenna Leopold <jleopold424@gmail.com>
2022-04-15 10:36:07 -0400

    use TrailRouter for '/trail'

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -4,6 +4,21 @@ const middleware = require('../middleware')
 
 router.post('/login', controler.Login)
 router.post('/register', controler.Register)
-router.put('/userId', middleware.stripToken, middleware.verifyToken, controler.UpdatePassword)
-router.get('/session', middleware.stripToken, middleware.verifyToken, controler.CheckSession)
-router.delete('/userId', middleware.stripToken, middleware.verifyToken, )
+router.put(
+  '/:userId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controler.UpdatePassword
+)
+router.get(
+  '/session',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controler.CheckSession
+)
+router.delete(
+  '/:userId',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controler.DeleteUser
+)
