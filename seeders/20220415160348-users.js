a64fe7610c2047e5142b6dc9570a62fdc8c5e95c
Molly Obermeier <obermeier.mol@gmail.com>
2022-04-21 20:57:47 -0600

    seed updates

--- seeders/20220415160348-users.js
+++ seeders/20220415160348-users.js
@@ -5,6 +5,10 @@ module.exports = {
     const nghiem = await middleware.hashPassword('nghiem')
     const jenna = await middleware.hashPassword('jenna')
     const molly = await middleware.hashPassword('molly')
+    const john = await middleware.hashPassword('john')
+    const michael = await middleware.hashPassword('michael')
+    const beyonce = await middleware.hashPassword('beyonce')
+    const grandma = await middleware.hashPassword('grandma')
     await queryInterface.bulkInsert('users', [
       {
         email: 'nghiem@nghiem.com',
@@ -27,6 +31,38 @@ module.exports = {
         name: 'Molly',
         trailName: 'Crunchy Granola',
         passwordDigest: molly,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'john@john.com',
+        name: 'John',
+        trailName: 'Speed Racer',
+        passwordDigest: john,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'michael@michael.com',
+        name: 'micheal',
+        trailName: 'Bear Necessities',
+        passwordDigest: michael,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'beyonce@beyonce.com',
+        name: 'beyonce',
+        trailName: 'Beeyonce',
+        passwordDigest: beyonce,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'grandma@grandma.com',
+        name: 'Grandma',
+        trailName: 'Grandma Gatewood',
+        passwordDigest: grandma,
         createdAt: new Date(),
         updatedAt: new Date()
       }
--- seeders/20220415160423-followers.js
+++ seeders/20220415160423-followers.js
@@ -38,6 +38,54 @@ module.exports = {
         follower_id: 2,
         createdAt: new Date(),
         updatedAt: new Date()
+      },
+      {
+        user_id: 1,
+        follower_id: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 2,
+        follower_id: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 3,
+        follower_id: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 1,
+        follower_id: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 4,
+        follower_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 5,
+        follower_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 6,
+        follower_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        user_id: 3,
+        follower_id: 7,
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ])
   },
